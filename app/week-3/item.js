4a98d0ac80f255ab2a4e081f5fb025b1a7a8a375
Hanish Jierdh <hanish.jierdh@edu.sait.ca>
2024-09-18 13:32:27 -0600

    Updated styling for the webpage using tailwind css

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,9 +1,8 @@
 export default function Item(props) {
     return(
-        <section>
-            <h2>{props.name}</h2>
-            <p>Quantity: {props.quantity}</p>
-            <p>Category: {props.category}</p>
+        <section className = "m-3 p-2 bg-neutral-700 text-neutral-100 w-96">
+            <h2 className = "font-bold text-neutral-100 text-lg">{props.name}</h2>
+            <p className = "text-neutral-100">Buy {props.quantity} in {props.category}</p>
         </section>
     );
 }
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,8 +2,8 @@ import ItemList
  from './item-list.js';
 export default function Page(){
     return(
-        <main>
-            <h1>Shopping List</h1>
+        <main className = "p-2 bg-neutral-900">
+            <h1 className = "text-3xl font-bold text-neutral-100">Shopping List</h1>
             <ItemList />
         </main>
     );
